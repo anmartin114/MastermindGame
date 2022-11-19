90032bf72abac3f8ee578c5535b6336d536e5709
Alicia Martin <anmartin114@gmail.com>
2022-11-18 21:03:29 -0500

    Add project files.

---
+++ Mastermind/GameMethods.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mastermind
+{
+    public static class GameMethods
+    {
+        static readonly Random random = new Random();
+
+        public static List<int> GetNumbers()
+        {
+            List<int> numbers = new List<int>();
+            for (int i = 0; i < 4; i++)
+            {
+                numbers.Add(random.Next(1, 6));
+            }
+            return numbers;
+        }
+
+        public static List<int> GetNumbersFromUser(string numbers)
+        {
+            List<int> usersNumbers = new List<int>();
+            char[] arr = numbers.ToCharArray();
+            foreach (char c in arr)
+            {
+                usersNumbers.Add(int.Parse(c.ToString()));
+            }
+            return usersNumbers;
+        }
+
+        public static List<char> CompareGuessToAnswer(List<int> numbersUser, List<int> numbersAnswer)
+        {
+            List<char> answer = new List<char>();
+            for (int i = 0; i < numbersUser.Count; i++)
+            {
+                if (numbersUser[i] == numbersAnswer[i])
+                {
+                    answer.Add('+');
+                }
+                else if (numbersAnswer.Contains(numbersUser[i]) && numbersUser[i] != numbersAnswer[i])
+                {
+                    answer.Add('-');
+                }
+                else
+                {
+                    answer.Add(' ');
+                }
+            }
+            answer.Sort();
+            return answer;
+        }
+
+        public static bool isCorrect(List<char> charList)
+        {
+            List<char> allCorrect = new List<char>();
+            for (int i = 0; i < 4; i++)
+            {
+                allCorrect.Add('+');
+            }
+
+            bool isEqual = Enumerable.SequenceEqual(allCorrect, charList);
+            if (isEqual)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public static bool IsValidNumber(string number)
+        {
+            if (int.TryParse(number, out int numberInt))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static string NumberListString(List<int> numbers)
+        {
+            string result = "";
+            foreach (int number in numbers)
+            {
+                result = result + number.ToString();
+            }
+            return result;
+        }
+    }
+}
+
---
+++ Mastermind/Program.cs
@@ -0,0 +1,42 @@
+﻿using Mastermind;
+
+Console.WriteLine("Welcome to Matermind!");
+Console.WriteLine("You have 10 attempts to guess a sequence of 4 numbers between 1 and 6");
+List<int> correctNumbers = GameMethods.GetNumbers();
+string correctNumberString = GameMethods.NumberListString(correctNumbers);
+int count = 0;
+bool win = false;
+
+while (count < 10)
+{
+    count++;
+    Console.WriteLine("\nEnter your guess: ");
+
+    string userInput = Console.ReadLine();
+
+    while (!GameMethods.IsValidNumber(userInput))
+    {
+        Console.WriteLine("Invalid number \n Enter your guess: ");
+        userInput = Console.ReadLine();
+    }
+
+    List<int> guessedNumbers = GameMethods.GetNumbersFromUser(userInput);
+    List<char> compareToAnswer = GameMethods.CompareGuessToAnswer(guessedNumbers, correctNumbers);
+
+    foreach (char c in compareToAnswer)
+    {
+        Console.Write(c);
+    }
+
+    if (GameMethods.isCorrect(compareToAnswer))
+    {
+        Console.WriteLine($"\nCorrect! You win! The secret number was: {correctNumberString}");
+        win = true;
+        break;
+    }
+}
+if (!win)
+{
+    Console.WriteLine("You couldn't guess the secret number in time :(");
+    Console.WriteLine($"The answer was: {correctNumberString}");
+}
